413e88962070efd247982e1e19c325cda4c3ecbd
Chetan Mehra <157389143+Chetan8383@users.noreply.github.com>
2024-03-19 22:17:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,219 +1,27 @@
 var swiper = new Swiper(".courses-slider", {
-    spaceBetween: 20,
-    grabCurser: true,
+    spaceBetween: 10,
+    grabCursor: true,
     // loop: true,
-    //freeMode: true,
 
 
     pagination: {
         el: ".swiper-pagination",
-        // dynamicBullets: true,
-        clickable: true,
+        dynamicBullets: true,
     },
 
     breakpoints: {
         540: {
-            slidesPerView: 1,
+            slidesPerView: 1.2,
         },
         768: {
             slidesPerView: 2.2,
         },
         1024: {
-            slidesPerView: 3.3,
+            slidesPerView: 3.2,
         },
     },
 });
 
-
-function myFunction1() {
-
-    document.getElementById("myDIV1").style.display = "block";
-
-    document.getElementById("myDIV2").style.display = "none";
-
-    document.getElementById("myDIV3").style.display = "none";
-
-    document.getElementById("myDIV4").style.display = "none";
-
-    document.getElementById("myDIV5").style.display = "none";
-
-    document.getElementById("myDIV6").style.display = "none";
-
-    document.getElementById("myDIV7").style.display = "none";
-
-    document.getElementById("myDIV8").style.display = "none";
-
-    document.getElementById("myDIV9").style.display = "none";
-
-}
-
-function myFunction2() {
-
-    document.getElementById("myDIV1").style.display = "none";
-
-    document.getElementById("myDIV2").style.display = "block";
-
-    document.getElementById("myDIV3").style.display = "none";
-
-    document.getElementById("myDIV4").style.display = "none";
-
-    document.getElementById("myDIV5").style.display = "none";
-
-    document.getElementById("myDIV6").style.display = "none";
-
-    document.getElementById("myDIV7").style.display = "none";
-
-    document.getElementById("myDIV8").style.display = "none";
-
-    document.getElementById("myDIV9").style.display = "none";
-}
-
-function myFunction3() {
-
-    document.getElementById("myDIV1").style.display = "none";
-
-    document.getElementById("myDIV2").style.display = "none";
-
-    document.getElementById("myDIV3").style.display = "block";
-
-    document.getElementById("myDIV4").style.display = "none";
-
-    document.getElementById("myDIV5").style.display = "none";
-
-    document.getElementById("myDIV6").style.display = "none";
-
-    document.getElementById("myDIV7").style.display = "none";
-
-    document.getElementById("myDIV8").style.display = "none";
-
-    document.getElementById("myDIV9").style.display = "none";
-}
-
-function myFunction4() {
-
-    document.getElementById("myDIV1").style.display = "none";
-
-    document.getElementById("myDIV2").style.display = "none";
-
-    document.getElementById("myDIV3").style.display = "none";
-
-    document.getElementById("myDIV4").style.display = "block";
-
-    document.getElementById("myDIV5").style.display = "none";
-
-    document.getElementById("myDIV6").style.display = "none";
-
-    document.getElementById("myDIV7").style.display = "none";
-
-    document.getElementById("myDIV8").style.display = "none";
-
-    document.getElementById("myDIV9").style.display = "none";
-}
-
-function myFunction5() {
-
-    document.getElementById("myDIV1").style.display = "none";
-
-    document.getElementById("myDIV2").style.display = "none";
-
-    document.getElementById("myDIV3").style.display = "none";
-
-    document.getElementById("myDIV4").style.display = "none";
-
-    document.getElementById("myDIV5").style.display = "block";
-
-    document.getElementById("myDIV6").style.display = "none";
-
-    document.getElementById("myDIV7").style.display = "none";
-
-    document.getElementById("myDIV8").style.display = "none";
-
-    document.getElementById("myDIV9").style.display = "none";
-}
-
-function myFunction6() {
-
-    document.getElementById("myDIV1").style.display = "none";
-
-    document.getElementById("myDIV2").style.display = "none";
-
-    document.getElementById("myDIV3").style.display = "none";
-
-    document.getElementById("myDIV4").style.display = "none";
-
-    document.getElementById("myDIV5").style.display = "none";
-
-    document.getElementById("myDIV6").style.display = "block";
-
-    document.getElementById("myDIV7").style.display = "none";
-
-    document.getElementById("myDIV8").style.display = "none";
-
-    document.getElementById("myDIV9").style.display = "none";
-}
-
-function myFunction7() {
-
-    document.getElementById("myDIV1").style.display = "none";
-
-    document.getElementById("myDIV2").style.display = "none";
-
-    document.getElementById("myDIV3").style.display = "none";
-
-    document.getElementById("myDIV4").style.display = "none";
-
-    document.getElementById("myDIV5").style.display = "none";
-
-    document.getElementById("myDIV6").style.display = "none";
-
-    document.getElementById("myDIV7").style.display = "block";
-
-    document.getElementById("myDIV8").style.display = "none";
-
-    document.getElementById("myDIV9").style.display = "none";
-}
-
-function myFunction8() {
-
-    document.getElementById("myDIV1").style.display = "none";
-
-    document.getElementById("myDIV2").style.display = "none";
-
-    document.getElementById("myDIV3").style.display = "none";
-
-    document.getElementById("myDIV4").style.display = "none";
-
-    document.getElementById("myDIV5").style.display = "none";
-
-    document.getElementById("myDIV6").style.display = "none";
-
-    document.getElementById("myDIV7").style.display = "none";
-
-    document.getElementById("myDIV8").style.display = "block";
-
-    document.getElementById("myDIV9").style.display = "none";
-}
-
-function myFunction9() {
-
-    document.getElementById("myDIV1").style.display = "none";
-
-    document.getElementById("myDIV2").style.display = "none";
-
-    document.getElementById("myDIV3").style.display = "none";
-
-    document.getElementById("myDIV4").style.display = "none";
-
-    document.getElementById("myDIV5").style.display = "none";
-
-    document.getElementById("myDIV6").style.display = "none";
-
-    document.getElementById("myDIV7").style.display = "none";
-
-    document.getElementById("myDIV8").style.display = "none";
-
-    document.getElementById("myDIV9").style.display = "block";
-}
 
 ////////////////////////STUDENTS PROJECT//////////////
 
