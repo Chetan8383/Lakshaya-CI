8ccb5909c4d18eeac978ef89f57ec665b966dbce
Chetan Mehra <157389143+Chetan8383@users.noreply.github.com>
2024-04-03 15:10:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var swiper = new Swiper(".courses-slider", {
     spaceBetween: 10,
     grabCursor: true,
@@ -33,4 +34,80 @@ var swiper = new Swiper(".students-projects-slider", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+});
+
+
+// ///////////////////////WEB_DEV_PROJECT///////////////////////////
+
+var swiper = new Swiper(".web-dev-project", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+    autoplay: {
+        delay: 250,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+});
+
+// /////////////WEB_DEV_PROJECT//////////
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //------ Slider Begin
+    const CaroS = document.querySelector('.web-dev-carousel');
+    const CaroSlider = new MicroSlider(CaroS, { indicators: true, indicatorText: '' });
+    const hammer = new Hammer(CaroS);
+    const CaroSTimer = 2000;
+    let CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+
+    //------- Mouseenter Event
+    CaroS.onmouseenter = function (e) {
+        clearInterval(CaroAutoplay);
+        console.log(e.type + ' mouse detected');
+    }
+
+    //----- Mouseleave Event
+    CaroS.onmouseleave = function (e) {
+        clearInterval(CaroAutoplay);
+        CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+        console.log(e.type + ' mouse detected');
+    }
+
+    //----- Mouseclick Event
+    CaroS.onclick = function (e) {
+        clearInterval(CaroAutoplay);
+        console.log(e.type + ' mouse detected');
+    }
+
+    //------ Gesture Tap Event
+    hammer.on('tap', function (e) {
+        clearInterval(CaroAutoplay);
+        console.log(e.type + ' gesture detected');
+    });
+
+    //----- Gesture Swipe Event
+    hammer.on('swipe', function (e) {
+        clearInterval(CaroAutoplay);
+        CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+        console.log(e.type + ' gesture detected');
+    });
+
 });
